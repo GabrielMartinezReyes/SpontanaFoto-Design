ca6dc1dc93fc129840367866d9bc4f9e6156d72c
Gabriel Martinez <gabriel.martinez.reyes@gmail.com>
2018-02-08 22:24:17 +0100

    Påbörjat inställningar för kollectionview

---
+++ SpontanaFoto&Design/SpontanaFoto&Design/Portfolio/PortfolioCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioCollectionViewCell.swift
+//  SpontanaFoto&Design
+//
+//  Created by Gabriel Martinez on 2018-02-08.
+//  Copyright © 2018 Gabriel Martinez. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    
+    
+    
+    
+    
+}
